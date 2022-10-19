c6da44352788bfbfd37603bdce74bf1d82b1514b
Quang Tran <quang.tran.769869@gmail.com>
2022-10-19 13:40:00 -0700

    added food class and detection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 import time
 from snake import Snake
+from food import Food
 screen = Screen()
 
 screen.setup(width = 600, height = 600)
@@ -9,6 +10,8 @@ screen.title("Py Snake")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -21,6 +24,10 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        print("nom nom nom")
+        food.refresh()
 
 
 screen.exitonclick()
